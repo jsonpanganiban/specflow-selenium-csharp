6c5d28c9f0e6c244c54a083f639ab97f757297a1
jsonpanganiban <jayson.panganiban@digibpl.com>
2020-02-16 14:01:59 +1100

    added login

---
+++ Jupiter.Tests/Pages/LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Jupiter.Tests.Pages
+{
+    public class LoginPage : Base
+    {
+        private readonly By usernameTextbox = By.CssSelector("#loginUserName");
+        private readonly By passwordTextbox = By.CssSelector("#loginPassword");
+        private readonly By loginButton = By.CssSelector(".btn-primary");
+        private readonly By loginErrorMessage = By.CssSelector("#login-error");
+        private readonly By loingForm = By.CssSelector("#loginForm");
+
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void Login(string username, string password)
+        {
+            Driver.FindElement(usernameTextbox).SendKeys(username);
+            Driver.FindElement(passwordTextbox).SendKeys(password);
+            Driver.FindElement(loginButton).Click();
+        }
+
+        public void ValidateLoginSuccessful()
+        {
+            Assert.True(!Driver.FindElement(loginErrorMessage).Displayed);
+        }
+
+        public void ValidateInvalidLogin(string message)
+        {
+            Assert.True(Driver.FindElement(loginErrorMessage).Displayed);
+            Assert.True(Driver.FindElement(loginErrorMessage).Text.ToString().Equals(message));
+        }
+    }
+}
---
+++ Jupiter.Tests/StepDefinitions/LoginSteps.cs
@@ -0,0 +1,36 @@
+﻿using Jupiter.Tests.Pages;
+using Jupiter.Tests.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Jupiter.Tests.StepDefinitions
+{
+    [Binding]
+    public class LoginSteps
+    {
+        private LoginPage loginPage;
+
+        public LoginSteps(LoginPage loginPage)
+        {
+            this.loginPage = loginPage;
+        }
+
+        [When(@"I enter invalid credentials")]
+        [When(@"I enter valid credentials")]
+        public void WhenIEnterCredentials(Table loginTable)
+        {
+            var loginDict = TableHelpersExtension.ConvertToDictionary(loginTable);
+            loginPage.Login(loginDict["Username"], loginDict["Password"]);
+        }
+
+        [Then(@"Message (.*) should be displayed")]
+        public void ThenFailureMessageShouldBeDisplayed(string message)
+        {
+            loginPage.ValidateInvalidLogin(message);
+        }
+    }
+}
